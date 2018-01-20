85b213fe2c07fed87001518fbeb781eb9480cb21
joe arnold <joearnold@joes-MacBook-Air.local>
2018-01-19 19:58:35 -0700

    friday

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,47 +4,65 @@ var queryURL = "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/B
 var userzip = 80210;
 var distances = [];
 var maxDistance = 150;
-var users = {};
-<<<<<<< HEAD
+var users = [];
 var userInput = 0;
 
+var customers = [
+{
+  name: "Joe Arnold",
+  email: "josephwilliamgj11@gmail.com",
+  zip: 77090
+},
+{
+  name: "Ken Lee",
+  email: "wewert@gmail.com",
+  zip: 80210
+},
+{
+  name: "Amy Christine",
+  email: "amychristine29@gmail.com",
+  zip: 80203
+},
+{
+  name: "Saijai Osika",
+  email: "hewjang@gmail.com",
+  zip: 80218
+}];
 
 
-$("#submit").on("click" , function(event) {
-  console.log("joe");
-=======
 
-$("#new-campaign-button").on("click" , function(event) {
-  console.log('joe');
->>>>>>> 730b3f650bfcf063a3a08bb7832e504494cf1e87
+// $("#submit").on("click" , function(event) {
+//   console.log("joe");
 
-$.each(zipcodes, function(i, v) {
+
+
+
+$.each(customers, function(i , v) {
   // distanceURL = "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=" + userzip + "&tozipcode=" + i + "&key=DEMOAPIKEY";
   $.ajax( {
-    url: "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=" + userzip + "&tozipcode=" + v + "&key=DEMOAPIKEY",
+    url: "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=" + userzip + "&tozipcode=" + v.zip + "&key=WXC5A3ACU3D13WNY58D7",
     method: "GET"
   }).done(function(response) { 
     var results = response.DistanceInMiles;
-    console.log(userzip);
+    // console.log(userzip);
   console.log('our results', i, v, results);
+  
+ 
 
 
-  if (results <= maxDistance) {
+  // if (results <= maxDistance) {
+    if (results <= maxDistance) {
+      
     users[i] = {
-      zip: v,
-      name: "person",
-      email: "something",
-      distance: results
+      zip: v.zip,
+      name: v.name,
+      email: v.email
+     
 
     //[variable]: value -- this will allow for variable based property names because of the square bracket notation
     }
   }
   console.log("people who we email", users);
-})
 });
 });
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 730b3f650bfcf063a3a08bb7832e504494cf1e87
+// });
