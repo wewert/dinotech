26f747fe3fd639fa57d7c52105dc1484370766d9
joe arnold <joearnold@joes-MacBook-Air.local>
2018-01-24 18:50:22 -0700

    joe

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,99 +22,146 @@ var maxDistance = "";
 var users = [];
 var userInput = 0;
 var campaignName = "";
+var customers = [];
 
-var customers = [
-  {
-    fname: "Joe",
-    lname: "Arnold",
-    email: "josephwilliamgj11@gmail.com",
-    zip: 77090
-  }
-  // {
-  //   fname: "Ken",
-  //   lname: "Lee",
-  //   email: "wewert@gmail.com",
-  //   zip: 80210
-  // },
-  // {
-  //   fname: "Amy",
-  //   lname: "Christine",
-  //   email: "amychristine29@gmail.com",
-  //   zip: 80203
-  // },
-  // {
-  //   fname: "Saijai",
-  //   lname: "Osika",
-  //   email: "hewjang@gmail.com",
-  //   zip: 80218
-  // }
-];
+// var customers = [
+//   {
+//     fname: "Joe",
+//     lname: "Arnold",
+//     email: "josephwilliamgj11@gmail.com",
+//     zip: 77090
+//   },
+//   {
+//     fname: "Ken",
+//     lname: "Lee",
+//     email: "wewert@gmail.com",
+//     zip: 80210
+//   },
+//   {
+//     fname: "Amy",
+//     lname: "Christine",
+//     email: "amychristine29@gmail.com",
+//     zip: 80203
+//   },
+//   {
+//     fname: "Joseph",
+//     lname: "Arnold",
+//     email: "joe@spotswork.com",
+//     zip: 90210
+//   },
+//   {
+//     fname: "Saijai",
+//     lname: "Osika",
+//     email: "hewjang@gmail.com",
+//     zip: 80218
+//   }
+// ];
+
+// function determineDistance() {
+//   $.each(customers, function(i, v) {
+//     userzip = $("#user-zip")
+//       .val()
+//       .trim();
+//     maxDistance = $("#max-distance")
+//       .val()
+//       .trim();
+//     campaignName = $("#campaign-name")
+//       .val()
+//       .toUpperCase()
+//       .trim();
+
+//     $.ajax({
+//       url:
+//         "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=" +
+//         userzip +
+//         "&tozipcode=" +
+//         v.zip +
+//         "&key=WXC5A3ACU3D13WNY58D7",
+//       method: "GET"
+//     }).done(function(response) {
+//       var results = response.DistanceInMiles;
+
+//       if (results <= maxDistance) {
+//         users[i] = {
+//           zip: v.zip,
+//           fname: v.fname,
+//           lname: v.lname,
+//           email: v.email,
+//           distance: results
+//         };
+   
+
+//       database.ref().push({
+//         campaignName: campaignName,
+//         zip: v.zip,
+//         fname: v.fname,
+//         lname: v.lname,
+//         email: v.email,
+//         distance: results
+//       });
+//     }
+
+    
+//       console.log("people who we email", users);
+//     });
+//   });
+// }
+
 
 function determineDistance() {
-  $.each(customers, function(i, v) {
-    userzip = $("#user-zip")
-      .val()
-      .trim();
-    maxDistance = $("#max-distance")
-      .val()
-      .trim();
-    campaignName = $("#campaign-name")
-      .val()
-      .toUpperCase()
-      .trim();
+    database.ref("/customers").on("child_added" , function(childSnapshot) {
+    customers = childSnapshot.val();
+    zip = customers.zip;
+    console.log(customers);
+    console.log(zip);
 
-    $.ajax({
-      url:
-        "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=" +
-        userzip +
-        "&tozipcode=" +
-        v.zip +
-        "&key=WXC5A3ACU3D13WNY58D7",
-      method: "GET"
+
+  $.each(customers, function(i, v) {
+    userzip = $("#user-zip").val().trim();
+    maxDistance = $("#max-distance").val().trim();
+    campaignName = $("#campaign-name").val().toUpperCase().trim();
+
+  $.ajax({
+    url: "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=" +
+    userzip + "&tozipcode=" + zip + "&key=1KD8MCQCP3U9ID1R4ETZ",
+    method: "GET"
+
     }).done(function(response) {
       var results = response.DistanceInMiles;
 
-      database.ref().push({
-        campaignName: campaignName,
-        zip: v.zip,
-        fname: v.fname,
-        lname: v.lname,
-        email: v.email,
-        distance: results
-      });
-
       if (results <= maxDistance) {
         users[i] = {
-          zip: v.zip,
-          fname: v.fname,
-          lname: v.lname,
-          email: v.email,
+          zip: customers.zip,
+          fname: customers.fname,
+          lname: customers.lname,
+          email: customers.email,
           distance: results
-
-          //[variable]: value -- this will allow for variable based property names because of the square bracket notation
         };
-      }
-      console.log("people who we email", users);
+
+          console.log("people who we email", users);
+        }
+      })
     });
   });
 }
+
+
 $(document).on("click", "#new-campaign-button", function(event) {
   event.preventDefault();
   determineDistance();
   console.log(userzip);
-  // $("#available-emails").prepend(name);
 });
 
 (function () {
                 var name = "Joseph";
-                var email = ["joe@spotswork.com" , "josephwilliamgj11@gmail.com" , "hewjang@gmail.com" , "wewert@gmail.com" , "amychristine29@gmail.com"];
+                var email = [];
                 var customer_name = "Dinosaur Lover";
 
 
                 emailjs.init("user_vRXnWslIHFZMq1MSBb3XD");
                 $("#form").submit(function (event) {
                     event.preventDefault();
-                  emailjs.send("jospehwilliamgj11_gmail_com", "template_sEGtEw5R", {
+                    emailjs.send("jospehwilliamgj11_gmail_com", "template_sEGtEw5R", {
                     to_email: email,
                     from_name: "Jurassic Quest!",
                     to_name: customer_name,
