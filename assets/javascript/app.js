ca8570bb9aac44ea946928ecfce20910eae1ddde
joe arnold <joearnold@joes-MacBook-Air.local>
2018-01-27 12:12:04 -0700

    sat

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ var database = firebase.database();
 // var zipcodes = [80210, 80203, 80218, 80205, 87114, 77090];
 // var testZip = 77090;
 var queryURL =
-  "http://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=80203&tozipcode=80210&key=DEMOAPIKEY";
+  "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=80203&tozipcode=80210&key=DEMOAPIKEY";
 var userzip = "";
 var distances = [];
 var maxDistance = "";
